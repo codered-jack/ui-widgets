394fe716c2a4b04318663805ba9ac4563ce38f74
codered-jack <sumitnbq@gmail.com>
2021-09-22 12:48:23 +0530

    IO

---
+++ intersectionObserver/src/index.js
@@ -0,0 +1,28 @@
+import "./style.scss";
+
+export const loadApp = () => {
+  const allp = document.querySelectorAll(".container p");
+
+  let options = {
+    root: null,
+    rootMargin: "-150px 50px",
+    threshold: 0.15,
+  };
+
+  let observer = new IntersectionObserver(touch, options);
+
+  allp.forEach((p) => observer.observe(p));
+
+  function touch(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+        //ob.unobserve(entry.target);
+      } else {
+        entry.target.classList.remove("active");
+      }
+    });
+  }
+};
+
+loadApp();
