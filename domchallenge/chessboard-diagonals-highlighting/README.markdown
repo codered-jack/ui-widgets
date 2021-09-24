fa80cda0d4d4d63e842fe9c5b4d7690fcd5ffff6
codered-jack <sumitnbq@gmail.com>
2021-09-24 19:09:18 +0530

    added edit

--- domchallenge/chessboard-diagonals-highlighting/README.markdown
+++
@@ -1,5 +0,0 @@
-# Chessboard Diagonals Highlighting
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/getify/pen/NWgawmO](https://codepen.io/getify/pen/NWgawmO).
-
-
--- domchallenge/draggabletodo/src/index.js
+++ domchallenge/draggabletodo/src/index.js
@@ -1,6 +1,54 @@
 let d = document.querySelector(".origin");
 let drop = document.getElementById("dropzone");
 let deleted = document.getElementById("deleted");
+
+let input = document.querySelector("input");
+
+let btn = document.getElementById("add");
+
+btn.addEventListener("click", addTodo);
+
+function addTodo() {
+  const completeBtn = document.createElement("button");
+  const deleteBtn = document.createElement("button");
+
+  completeBtn.innerHTML = "Complete";
+  deleteBtn.innerHTML = "Delete";
+
+  let newlist = document.createElement("div");
+
+  let newlistitem = document.createElement("div");
+  newlistitem.setAttribute("contenteditable", "true");
+
+  newlistitem.innerText = input.value;
+
+  newlist.setAttribute("class", "draggable");
+  newlist.setAttribute("id", Math.floor(Math.random() * 20));
+  newlist.setAttribute("draggable", "true");
+  newlist.appendChild(newlistitem);
+  newlist.appendChild(completeBtn);
+  newlist.appendChild(deleteBtn);
+  if (input.value !== "") {
+    d.appendChild(newlist);
+    input.value = "";
+  }
+
+  completeBtn.addEventListener("click", function () {
+    const parent = this.parentNode;
+    parent.remove();
+    drop.appendChild(parent);
+    completeBtn.style.display = "none";
+  });
+
+  deleteBtn.addEventListener("click", function () {
+    const parent = this.parentNode;
+    parent.remove();
+    deleted.appendChild(parent);
+    deleteBtn.style.display = "none";
+    completeBtn.style.display = "none";
+  });
+}
+
 d.addEventListener("dragstart", (e) => {
   e.dataTransfer.setData("dragdata", e.target.id);
   e.target.style.background = "yellow";
@@ -14,6 +62,10 @@ drop.addEventListener("dragover", (e) => {
 drop.addEventListener("drop", (e) => {
   const data = e.dataTransfer.getData("dragdata");
   const dragElem = document.getElementById(data);
+  let child = dragElem.children;
+
+  child[1].style.display = "none";
+  child[2].style.display = "block";
   drop.appendChild(dragElem);
 });
 
@@ -25,6 +77,10 @@ deleted.addEventListener("dragover", (e) => {
 deleted.addEventListener("drop", (e) => {
   const data = e.dataTransfer.getData("dragdata");
   const dragElem = document.getElementById(data);
+  let child = dragElem.children;
+
+  child[1].style.display = "none";
+  child[2].style.display = "none";
   deleted.appendChild(dragElem);
 });
 
@@ -42,6 +98,10 @@ d.addEventListener("dragover", (e) => {
 d.addEventListener("drop", (e) => {
   const data = e.dataTransfer.getData("dragdata");
   const dragElem = document.getElementById(data);
+  let child = dragElem.children;
+
+  child[1].style.display = "block";
+  child[2].style.display = "block";
   d.appendChild(dragElem);
 });
 
